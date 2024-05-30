27e80030755b39cabe5af2d8615d2f7ce7fbf73d
Sugar0612 <287276293@qq.com>
2024-05-30 22:41:20 +0800

    chore: 添加预编译文件。

---
+++ SRAssistant/Launcher/MainWin.cpp
@@ -0,0 +1,9 @@
+#include "../Precompiled.h"
+
+#pragma warning(disable:4996) // ºöÂÔ4996´íÎóÌáÊ¾
+#pragma comment(linker, "/entry:mainCRTStartup /subsystem:windows")
+
+int main()
+{
+	return 0;
+}
---
+++ SRAssistant/Precompiled.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <thread>
+#include <vector>
